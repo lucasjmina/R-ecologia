bb78dae0dc30f34958bc7d9ecf616b0dfb94a9a4
LucasJM <lucas_git.nvgw4@slmail.me>
2025-11-07 23:30:59 -0300

    Initial commit

---
+++ _main.R
@@ -0,0 +1,6 @@
+# if (!requireNamespace("renv", quietly = TRUE)) {
+#   install.packages("renv", repos = "https://cloud.r-project.org")
+# }
+# renv::restore()
+library(here)
+source(here("src", "_init.R"))
---
+++ src/_init.R
@@ -0,0 +1,1 @@
+project_env <- new.env()
