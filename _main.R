b88791d0fb021770b075542f647269822de98c90
LucasJM <lucas_git.nvgw4@slmail.me>
2025-11-10 02:31:00 -0300

    Unidades 01 a 03, configuraciones para sitio web, datos

--- _main.R
+++
@@ -1,6 +0,0 @@
-# if (!requireNamespace("renv", quietly = TRUE)) {
-#   install.packages("renv", repos = "https://cloud.r-project.org")
-# }
-# renv::restore()
-library(here)
-source(here("src", "_init.R"))
